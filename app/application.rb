fa775e20037ce1f0fc3167e2cd8ac22aab4de56a
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-30 04:17:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,9 +15,9 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
     elsif req.path.match(/add/)
       search_term = req.params["item"]
       resp.write handle_add(search_term)
