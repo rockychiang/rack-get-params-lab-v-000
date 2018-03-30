099e3150c0ead5413d304788c7a9cd2e2a2ae148
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-30 04:18:21 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,6 +16,7 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       if @@cart.empty?
+        
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
