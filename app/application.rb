dce77335a08a84d935973371e8114d7ae5e74667
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-30 04:13:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,6 +18,8 @@ class Application
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/add/)
+        
     else
       resp.write "Path Not Found"
     end
